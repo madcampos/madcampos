e1dccc82bb1e19529d18ab274721c8133a29758b
madcampos <me@madcampos.dev>
2025-05-15 00:20:12 -0400

    feat: add click handler for buttons

---
+++ src/components/OldStyleButtons/index.ts
@@ -0,0 +1,21 @@
+class OldStyleButton extends HTMLElement {
+	constructor() {
+		super();
+
+		const supportsDeclarative = Object.hasOwn(HTMLElement.prototype, 'attachInternals');
+		const internals = supportsDeclarative ? this.attachInternals() : undefined;
+		const shadow = internals?.shadowRoot ?? this.attachShadow({ mode: 'open' });
+
+		shadow.querySelector('button')?.addEventListener('click', () => {
+			const dest = this.getAttribute('dest') ?? '';
+
+			if (dest) {
+				window.open(new URL(dest, import.meta.url).toString(), '_blank', 'noopener,noreferrer');
+			}
+		});
+	}
+}
+
+if (!customElements.get('old-style-button')) {
+	customElements.define('old-style-button', OldStyleButton);
+}
