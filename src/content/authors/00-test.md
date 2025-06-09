9fb30f947bcf44a401696e538d1370a979e357f0
madcampos <me@madcampos.dev>
2025-06-08 20:42:19 -0400

    feat: add author handling of markdown and drafts

---
+++ src/content/authors/00-test.md
@@ -0,0 +1,29 @@
+---
+name: 000 TEST <html> & **_MD_**
+draft: true
+avatar: ./assets/00-test.png
+avatarAlt: TEST <html> & **_MD_**
+email: test@example.com
+website: https://example.com/
+socialMedia:
+  Github: https://example.com
+  CodePen: https://example.com
+  LinkedIn: https://example.com
+---
+
+### Test markdown content
+
+This is a **bold** text and this is an _italic_ text.
+Here is a [link](https://example.com) and an inline `code` sample.
+This text is ==highlighted==.
+~~Strikethrough (deleted) text~~
+++Underline (inserted) text++
+
+Here is an image:
+
+![Alt text](./assets/00-test.png)
+
+### Task list
+
+- [ ] Unchecked task
+- [x] Checked task
--- src/utils/authors.ts
+++ src/utils/authors.ts
@@ -1,14 +1,16 @@
 import { getCollection, getEntry, render } from 'astro:content';
 
 export async function listAllAuthors() {
-	const authorEntries = await getCollection('authors');
-	const authors = authorEntries.sort((first, second) => first.data.name.localeCompare(second.data.name, 'en-US', { usage: 'sort' }));
-	const authorsWithRender = authors.map((author) => ({
-		...author,
-		render: async () => render(author)
-	}));
+	const collectionEntries = await getCollection('authors');
+	const entries = collectionEntries
+		.filter(({ data: { draft } }) => !draft || import.meta.env.DEV)
+		.sort((first, second) => first.data.name.localeCompare(second.data.name, 'en-US', { usage: 'sort' }))
+		.map((author) => ({
+			...author,
+			render: async () => render(author)
+		}));
 
-	return authorsWithRender;
+	return entries;
 }
 
 export async function getAuthor(authorId: string) {
