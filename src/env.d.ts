f78a2f55770d3da4212bd9354199893c92ca925d
madcampos <madcampos@outlook.com>
2025-07-18 18:47:00 -0400

    fix: build issue with icons

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference path="../.astro/icon.d.ts" />
 
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pwa/info" />
