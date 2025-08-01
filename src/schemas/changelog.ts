6142eb5bcdd1ab9b2c656d4a7f3973ffe3a74dfa
madcampos <me@madcampos.dev>
2025-07-31 23:54:29 -0400

    feat: move content files

--- src/schemas/changelog.ts
+++
@@ -1,8 +0,0 @@
-import { z as zod } from 'astro:content';
-
-export const changelogSchema = () =>
-	zod.object({
-		versionName: zod.string().optional().describe('The version name.'),
-		date: zod.date().describe('The version release date.'),
-		draft: zod.boolean().optional().describe('If this version is a draft.')
-	});
