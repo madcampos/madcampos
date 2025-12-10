c8ed2384bddd21e4a365b724a9c4dd98f6e7c471
madcampos <madcampos@outlook.com>
2025-12-10 18:11:53 -0500

    feat: update router for server

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,15 @@
-/// <reference path="./env.d.ts" />
+import { Router } from './router.ts';
+import { nonExistentPages } from './routes/410.ts';
+import { yapper } from './routes/yapper/index.ts';
 
-export default {
-	async fetch(request) {
-		if (!new URL(request.url).pathname.startsWith('/api')) {
-			return new Response('Get out, you are durnk!', { status: 410 });
-		}
-
-		return new Response('pong!', { status: 200 });
+const router = new Router({
+	routes: {
+		'/tojs-styleguide': nonExistentPages,
+		'/tojs-styleguide/': nonExistentPages,
+		'/blog/2023/06/07/tabs-web-component': nonExistentPages,
+		'/blog/2023/06/07/tabs-web-component/': nonExistentPages,
+		'/yapping/*': yapper
 	}
-} satisfies ExportedHandler<Env>;
+});
+
+export default router satisfies ExportedHandler<Env>;
--- server/router.ts
+++ server/router.ts
@@ -1,114 +1,30 @@
-/* eslint-disable @typescript-eslint/no-invalid-void-type */
+export type ResponseHandler = (request: Request, env: Env, context: ExecutionContext) => Promise<Response> | Response;
 
-type IsParameter<Part> = Part extends `:${infer ParamName}` ? ParamName : never;
-
-type FilteredParts<Path> = Path extends `${infer PartA}/${infer PartB}` ? FilteredParts<PartB> | IsParameter<PartA>
-	: IsParameter<Path>;
-
-type Params<Path> = Record<FilteredParts<Path>, string>;
-
-export interface RouteLocation<Path = string> {
-	path: Path;
-	params: Params<Path>;
-	query?: Record<string, string | undefined>;
-	hash?: string;
-}
-
-type ResponseHandler = (request: Request) => Response | Promise<Response>;
-
-interface RouteDefinition {
-	path: string;
-	handler: ResponseHandler;
-}
-
-interface RouterConfig {
-	routes: RouteDefinition[];
-	fallback?: ResponseHandler;
+interface StaticSiteHandlerOptions {
+	routes: Record<string, ResponseHandler>;
+	fallbackRoute?: ResponseHandler;
 }
 
 export class Router {
-	static #routes: [URLPattern, ResponseHandler][] = [];
-	static readonly #fallbackPattern = new URLPattern({ pathname: '*' });
+	#routes: [URLPattern, ResponseHandler][] = [];
+	#fallbackRoute: ResponseHandler;
 
-	static fallback: ResponseHandler | undefined;
+	constructor({ routes, fallbackRoute }: StaticSiteHandlerOptions) {
+		this.#fallbackRoute = fallbackRoute ?? (() => new Response('Not Found', { status: 404, headers: { 'Content-Type': 'text/plain' } }));
 
-	static add<T extends ViewImplementation>(path: string, ViewClass: T) {
-		const view = new ViewClass();
-
-		view.dataset['routerView'] = '';
-
-		Router.#routes.push([new URLPattern({ pathname: path }), view]);
-
-		Router.renderTarget.appendChild(view as unknown as Node);
+		Object.entries(routes).forEach(([path, route]) => {
+			this.#routes.push([new URLPattern({ pathname: path }), route]);
+		});
 	}
 
-	static async navigate(request: Request) {
-		try {
-			const newPath = new URL(path, Router.#baseUrl).pathname;
+	async fetch<CfHostMetadata = unknown>(request: Request<CfHostMetadata, IncomingRequestCfProperties<CfHostMetadata>>, env: Env, context: ExecutionContext) {
+		const resolvedUrl = new URL(request.url);
+		const [pattern, routeHandler] = this.#routes.find(([curPattern]) => curPattern.test(resolvedUrl.href)) ?? [];
 
-			if (Router.#currentPath === newPath) {
-				return;
-			}
-
-			const guardResult = await Router.beforeEach?.(this.#currentPath, newPath);
-
-			if (guardResult === false) {
-				return;
-			}
-
-			const pathToSearch = guardResult?.path ?? newPath;
-
-			const [matcher, view] = Router.#routes.find(([pattern]) => pattern.test(pathToSearch, this.#baseUrl)) ?? [];
-
-			if (matcher !== undefined && view) {
-				const destinationMatcher = matcher.exec(pathToSearch, Router.#baseUrl);
-				const destination: RouteLocation = {
-					path: pathToSearch,
-					params: destinationMatcher?.pathname.groups ?? {},
-					query: destinationMatcher?.search.groups ?? {},
-					hash: destinationMatcher?.hash.input
-				};
-
-				const title = await view.navigate?.(destination, Router.#currentLocation) ?? undefined;
-
-				Router.#routes.forEach(([, otherView]) => {
-					delete otherView.dataset['activeView'];
-				});
-
-				view.dataset['activeView'] = '';
-
-				Router.#currentPath = pathToSearch;
-				Router.#currentLocation = destination;
-
-				const basePath = new URL(Router.#baseUrl).pathname.replace(/\/$/u, '');
-				const normalizedPath = new URL(`${basePath}${path}`, Router.#baseUrl).pathname;
-
-				window.history.pushState(null, '', normalizedPath);
-
-				if (title) {
-					window.document.title = `${title} · ${Router.appTitle}`;
-				} else {
-					window.document.title = Router.appTitle;
-				}
-			}
-		} catch (err) {
-			console.error(`[⛵️] Error while navigating to ${path}:`, err);
-		}
-	}
-
-	static init({
-		routes,
-		fallback
-	}: RouterConfig) {
-		routes.forEach(({ path, handler: view }) => Router.add(path, view));
-
-		if (fallback) {
-			// TODO: add direct handler to '*'
-			// Router.fallback = fallback;
+		if (!pattern || !routeHandler) {
+			return this.#fallbackRoute(request, env, context);
 		}
 
-		console.info('[⛵️] Router initialized');
+		return routeHandler(request, env, context);
 	}
 }
-
-import './router-link';
