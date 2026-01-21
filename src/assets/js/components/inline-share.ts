0653700bf66d7183ae1a855a3b1abda516d6d8db
madcampos <me@madcampos.dev>
2026-01-21 03:16:17 -0500

    feat: improve implementation for inline share

--- src/assets/js/components/inline-share.ts
+++ src/assets/js/components/inline-share.ts
@@ -1,12 +1,20 @@
 import { SiteSettings } from '../settings.ts';
 
+interface DrawTextFontStyle {
+	color?: string;
+	alignment?: CanvasTextAlign;
+	fontFamily?: string;
+	style?: 'bold' | 'italic' | 'normal';
+	size?: number;
+}
+
 export class InlineShare extends HTMLElement implements CustomElement {
 	readonly #MIN_WORDS_FOR_SHARING = 3;
-	readonly #MAX_WORDS_FOR_SHARING = 25;
+	readonly #MAX_WORDS_FOR_SHARING = 30;
 
-	readonly #TEXT_BOX_WIDTH = 980;
-	readonly #TEXT_BOX_TOP_OFFSET = 50;
-	readonly #TEXT_BOX_LINE_HEIGHT = 60;
+	// TODO: calculate sizes as percentage to the total size
+	readonly #TEXT_BOX_WIDTH = 950;
+	readonly #TEXT_BOX_TOP_OFFSET = 120;
 
 	#id: string;
 
@@ -42,6 +50,32 @@ export class InlineShare extends HTMLElement implements CustomElement {
 				<dialog-content>
 					<canvas></canvas>
 				</dialog-content>
+				<footer>
+					<button type="button" class="share-os-link">
+						<sr-only>Share Link to Quote</sr-only>
+						<svg viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" data-icon>
+							<use href="#share-icon-share" width="24" height="24" />
+						</svg>
+					</button>
+					<button type="button" class="share-copy">
+						<sr-only>Copy Link to Quote</sr-only>
+						<svg viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" data-icon>
+							<use href="#share-icon-copy" width="24" height="24" />
+						</svg>
+					</button>
+					<button type="button" class="share-email">
+						<sr-only>Share via Email</sr-only>
+						<svg viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" data-icon>
+							<use href="#share-icon-email" width="24" height="24" />
+						</svg>
+					</button>
+					<button type="button" class="share-download">
+						<sr-only>Download quote</sr-only>
+						<svg viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" data-icon>
+							<use href="#share-icon-download" width="24" height="24" />
+						</svg>
+					</button>
+				</footer>
 			</dialog>
 		`;
 
@@ -52,14 +86,15 @@ export class InlineShare extends HTMLElement implements CustomElement {
 		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 		this.#canvasContext = this.#canvas.getContext('2d')!;
 
+		this.#canvasContext.imageSmoothingEnabled = true;
+		this.#canvasContext.imageSmoothingQuality = 'high';
+
 		this.#resizeCanvas(1024, 1024);
 	}
 
 	#resizeCanvas(width: number, height: number) {
-		this.#canvas.width = width;
-		this.#canvas.height = height;
-		this.#canvasContext.canvas.width = this.#canvas.width;
-		this.#canvasContext.canvas.height = this.#canvas.height;
+		this.#canvas.width = width * window.devicePixelRatio;
+		this.#canvas.height = height * window.devicePixelRatio;
 	}
 
 	#drawBackground() {
@@ -71,54 +106,75 @@ export class InlineShare extends HTMLElement implements CustomElement {
 		this.#canvasContext.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
 	}
 
-	#drawText(text: string) {
-		this.#canvasContext.font = '50px monospace';
-		this.#canvasContext.textAlign = 'center';
-		this.#canvasContext.fillStyle = 'white';
+	#drawText(
+		text: string,
+		x = this.#canvas.width / 2,
+		y = this.#TEXT_BOX_TOP_OFFSET,
+		{ fontFamily = "Mecano-Light', 'Mecano', monospace", size = 50, alignment = 'center', color = 'white', style = 'italic' }: DrawTextFontStyle = {}
+	) {
+		this.#canvasContext.font = `${style} ${size}px ${fontFamily}`;
+		this.#canvasContext.textAlign = alignment;
+		this.#canvasContext.fillStyle = color;
 
 		const segments = Array.from(this.#segmenter.segment(text));
-		const words = segments.slice(0, this.#MAX_WORDS_FOR_SHARING).map((segment) => segment.segment);
 		const lines: string[] = [];
 		let currentLine = '';
 		let currentWidth = 0;
+		let currentWords = 0;
 
-		// TODO: check for spaces and line breaks
-		for (const word of words) {
-			const wordWidth = this.#canvasContext.measureText(word).width;
-
-			if (currentWidth + wordWidth >= this.#TEXT_BOX_WIDTH) {
-				lines.push(currentLine.trim());
-				currentLine = `${word} `;
-				currentWidth = wordWidth;
-			} else {
-				currentLine += `${word} `;
-				currentWidth += wordWidth;
+		for (const segment of segments) {
+			if (segment.isWordLike) {
+				currentWords += 1;
 			}
-		}
 
-		if (segments.length > this.#MAX_WORDS_FOR_SHARING) {
-			currentLine += '...';
+			if (currentWords > this.#MAX_WORDS_FOR_SHARING) {
+				currentLine = `${currentLine.trim()}...`;
+				break;
+			}
+
+			if (segment.segment === '\n') {
+				lines.push(currentLine.trim());
+				currentLine = '';
+				currentWidth = 0;
+			} else {
+				const wordWidth = this.#canvasContext.measureText(segment.segment).width;
+
+				if (currentWidth + wordWidth >= this.#TEXT_BOX_WIDTH) {
+					lines.push(currentLine.trim());
+					currentLine = segment.segment;
+					currentWidth = wordWidth;
+				} else {
+					currentLine += segment.segment;
+					currentWidth += wordWidth;
+				}
+			}
 		}
 
 		if (currentLine) {
 			lines.push(currentLine.trim());
 		}
 
-		let y = this.#TEXT_BOX_TOP_OFFSET;
+		const { actualBoundingBoxAscent: ascent, actualBoundingBoxDescent: descent } = this.#canvasContext.measureText('EM');
+		const lineHeight = ascent + descent;
 
+		let currentY = y;
 		for (const line of lines) {
-			this.#canvasContext.fillText(line, this.#canvas.width / 2, y, this.#TEXT_BOX_WIDTH);
-			y += this.#TEXT_BOX_LINE_HEIGHT;
+			this.#canvasContext.fillText(line, x, currentY);
+			currentY += lineHeight;
 		}
 	}
 
 	#renderToCanvas(text: string) {
 		this.#drawBackground();
+
+		// TODO: improve styling.
+		this.#drawText('Marco Campos', this.#canvas.width - 10, this.#canvas.height - 50, { color: 'red', alignment: 'right', size: 20 });
+		this.#drawText(document.title, this.#canvas.width - 10, this.#canvas.height - 20, { color: 'red', alignment: 'right', size: 20 });
+
 		this.#drawText(text);
 	}
 
 	#resizeOverlay(range: Range) {
-		// TODO: compute the correct size based on all of the rects for the range.
 		const { top, left, width, height } = range.getBoundingClientRect();
 		const { scrollX, scrollY } = window;
 
@@ -129,14 +185,40 @@ export class InlineShare extends HTMLElement implements CustomElement {
 	}
 
 	#handleSelectionChange() {
+		if (this.querySelector('dialog')?.matches(':popover-open')) {
+			return;
+		}
+
+		this.hidden = true;
+
 		const selection = document.getSelection();
 
 		if (!selection) {
 			return;
 		}
 
+		if (selection.rangeCount < 1) {
+			return;
+		}
+
+		const range = selection.getRangeAt(0);
+		const ancestor = range.commonAncestorContainer;
+		const isRenderedContent = (ancestor as Element)?.matches?.('rendered-content') ?? false;
+		const isInsideRenderedContent = Boolean(ancestor.parentElement?.closest('rendered-content'));
+
+		if (!isRenderedContent && !isInsideRenderedContent) {
+			return;
+		}
+
+		const isCodeBlock = (ancestor as Element)?.matches?.('code-wrapper') ?? false;
+		const isInsideCodeBlock = Boolean(ancestor.parentElement?.closest('code-wrapper'));
+
+		if (isCodeBlock || isInsideCodeBlock) {
+			return;
+		}
+
 		if (Array.from(this.#segmenter.segment(selection.toString())).length >= this.#MIN_WORDS_FOR_SHARING) {
-			this.#resizeOverlay(selection.getRangeAt(0));
+			this.#resizeOverlay(range);
 			this.#renderToCanvas(selection.toString());
 
 			this.hidden = false;
@@ -146,6 +228,7 @@ export class InlineShare extends HTMLElement implements CustomElement {
 	}
 
 	handleEvent(evt: Event) {
+		// TODO: add button click events
 		if (evt.type === 'selectionchange') {
 			this.#handleSelectionChange();
 		}
