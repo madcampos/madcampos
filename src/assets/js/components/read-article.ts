ce1fe4de616c020dd3cd10c9bd02a3a711ad4f47
Marco Campos <609904+madcampos@users.noreply.github.com>
2026-03-04 13:53:17 -0500

    feat: add TODO component

---
+++ src/assets/js/components/read-article.ts
@@ -0,0 +1,8 @@
+// TODO: implement article reading component
+// 1. Get list of voices
+// 2. Group list by language, local/online; sort by name; show default first. (hide other languages?)
+// 3. Get article elements, split by element type like code or paragraphs
+// 4. Infer element language
+// 5. Set global pitch, speed, and volume
+// 6. Add event to pause on voice change
+// 7. Add event for boundaries and implement highlight of word when the boundary change
