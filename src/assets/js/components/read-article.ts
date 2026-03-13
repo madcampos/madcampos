ab366cb62b4bd3e06c3b93dd042b2f51c6ef220a
madcampos <609904+madcampos@users.noreply.github.com>
2026-03-13 02:04:58 -0400

    chore: add TODOs

--- src/assets/js/components/read-article.ts
+++ src/assets/js/components/read-article.ts
@@ -1,13 +1,3 @@
-// TODO: implement article reading component
-// Ref: https://blog.damato.design/_astro/hoisted.C11L7A5c.js
-// 1. Get list of voices
-// 2. Group list by language, local/online; sort by name; show default first. (hide other languages?)
-// 3. Get article elements, split by element type like code or paragraphs
-// 4. Infer element language
-// 5. Set global pitch, speed, and volume
-// 6. Add event to pause on voice change
-// 7. Add event for boundaries and implement highlight of word when the boundary change
-
 import { SiteSettings } from '../settings.ts';
 
 interface SectionUtterance {
@@ -257,6 +247,7 @@ class ReadArticle extends HTMLElement implements CustomElement {
 	}
 
 	render() {
+		// TODO: improve markup
 		this.innerHTML = `
 			<form action="" method="post">
 				<button type="button" name="read-article">Read article</button>
