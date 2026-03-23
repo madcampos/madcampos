206c6a58f13304d0343360f17db62e7c31db10c1
madcampos <609904+madcampos@users.noreply.github.com>
2026-03-23 00:42:28 -0400

    feat: add missing data attribute for relative time

--- src/assets/js/components/relative-time.ts
+++ src/assets/js/components/relative-time.ts
@@ -7,23 +7,23 @@ const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
 function getRelativeTime(date: Date): string {
 	const now = new Date();
 
-	const diffInMs = Math.abs(date.getTime() - now.getTime());
-	const diffInSeconds = Math.abs(Math.round(diffInMs / 1000));
-	const diffInMinutes = Math.abs(Math.round(diffInMs / (1000 * 60)));
-	const diffInHours = Math.abs(Math.round(diffInMs / (1000 * 60 * 60)));
-	const diffInDays = Math.abs(Math.round(diffInMs / (1000 * 60 * 60 * 24)));
-	const diffInMonths = Math.abs(Math.round(diffInMs / (1000 * 60 * 60 * 24 * 30)));
-	const diffInYears = Math.abs(Math.round(diffInMs / (1000 * 60 * 60 * 24 * 365)));
+	const diffInMs = date.getTime() - now.getTime();
+	const diffInSeconds = Math.round(diffInMs / 1000);
+	const diffInMinutes = Math.round(diffInMs / (1000 * 60));
+	const diffInHours = Math.round(diffInMs / (1000 * 60 * 60));
+	const diffInDays = Math.round(diffInMs / (1000 * 60 * 60 * 24));
+	const diffInMonths = Math.round(diffInMs / (1000 * 60 * 60 * 24 * 30));
+	const diffInYears = Math.round(diffInMs / (1000 * 60 * 60 * 24 * 365));
 
-	if (diffInSeconds < 60) {
+	if (Math.abs(diffInSeconds) < 60) {
 		return rtf.format(diffInSeconds, 'second');
-	} else if (diffInMinutes < 60) {
+	} else if (Math.abs(diffInMinutes) < 60) {
 		return rtf.format(diffInMinutes, 'minute');
-	} else if (diffInHours < 24) {
+	} else if (Math.abs(diffInHours) < 24) {
 		return rtf.format(diffInHours, 'hour');
-	} else if (diffInDays < 30) {
+	} else if (Math.abs(diffInDays) < 30) {
 		return rtf.format(diffInDays, 'day');
-	} else if (diffInMonths < 12) {
+	} else if (Math.abs(diffInMonths) < 12) {
 		return rtf.format(diffInMonths, 'month');
 	}
 
@@ -36,7 +36,7 @@ if (SiteSettings.js !== 'disabled') {
 
 		if (datetime) {
 			const date = new Date(datetime);
-			element.textContent += ` (${getRelativeTime(date)})`;
+			element.textContent = `${element.textContent} (${getRelativeTime(date)})`;
 		}
 	});
 }
