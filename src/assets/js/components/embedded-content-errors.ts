23eda102e37355c19cb0e5a1ad05be7bfbea2a2c
Marco Campos <609904+madcampos@users.noreply.github.com>
2026-04-13 13:45:48 -0400

    fix: mark all js files as modules

--- src/assets/js/components/embedded-content-errors.ts
+++ src/assets/js/components/embedded-content-errors.ts
@@ -1,5 +1,3 @@
-export type ModulePlaceholder = unknown;
-
 document.querySelectorAll('img, audio, video, iframe').forEach((embed) => {
 	embed.addEventListener('error', () => {
 		embed.toggleAttribute('data-load-error', true);
