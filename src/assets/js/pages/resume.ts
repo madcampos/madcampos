3b9123fab61136040c2db1f8b0070de40defe272
Marco Campos <609904+madcampos@users.noreply.github.com>
2026-04-15 16:53:53 -0400

    feat: update resume

--- src/assets/js/pages/resume.ts
+++ src/assets/js/pages/resume.ts
@@ -1,10 +1,16 @@
-const CUTOUT_YEAR = 5;
+const CUTOUT_YEAR = 7;
 const cutoutDate = new Date();
 
 cutoutDate.setFullYear(cutoutDate.getFullYear() - CUTOUT_YEAR);
 
-document.querySelectorAll<HTMLTimeElement>('[data-enddate]').forEach((element) => {
-	const dateTime = new Date(element.dateTime);
+document.querySelectorAll<HTMLDivElement>('div:not([data-old-entry]):has(time[data-enddate])').forEach((element) => {
+	const timeElement = element.querySelector<HTMLTimeElement>('time[data-enddate]');
+
+	if (!timeElement) {
+		return;
+	}
+
+	const dateTime = new Date(timeElement.dateTime);
 
 	if (cutoutDate.getFullYear() < dateTime.getFullYear()) {
 		return;
