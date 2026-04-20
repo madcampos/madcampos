d2de111df317d51ff2a368962846ae822749ba76
Marco Campos <609904+madcampos@users.noreply.github.com>
2026-04-20 19:00:39 -0400

    feat: move yapper to be static

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,9 @@
 import { Router } from './router.ts';
-import { nonExistentPages } from './routes/410.ts';
 import { getVisitorCount, incrementVisitorCount } from './routes/hit-counter.ts';
-import { yapper } from './routes/yapper/index.ts';
 import { simpleOptionsResponse } from './utils/index.ts';
 
 const router = new Router({
 	routes: {
-		'GET /tojs-styleguide': nonExistentPages,
-		'GET /tojs-styleguide/': nonExistentPages,
-		'GET /blog/2023/06/07/tabs-web-component': nonExistentPages,
-		'GET /blog/2023/06/07/tabs-web-component/': nonExistentPages,
-		'GET /yapping': yapper,
-		'GET /yapping/*': yapper,
 		'OPTIONS /api/counter/': simpleOptionsResponse,
 		'GET /api/counter/': getVisitorCount,
 		'PUT /api/counter/': incrementVisitorCount
--- server/routes/410.ts
+++
@@ -1,3 +0,0 @@
-export function nonExistentPages() {
-	return new Response('Page does not exist', { status: 410, statusText: 'page' });
-}
