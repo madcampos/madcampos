e8c768a0a89516f248e2bb0381ecb84d142185b6
madcampos <609904+madcampos@users.noreply.github.com>
2026-07-17 19:57:05 -0400

    fix: update logic for custom element style loading

--- src/assets/js/custom-element.ts
+++ src/assets/js/custom-element.ts
@@ -1,30 +1,44 @@
-export async function loadComponentCss(tagName: string, stylesUrl: string) {
-	return new Promise((resolve, reject) => {
-		const linkSelector = `link[rel="stylesheet"][data-component="${tagName}"]`;
+// oxlint-disable-next-line no-magic-numbers
+const LOADING_TIMEOUT = 5 * 1000;
 
-		if (document.head.querySelector(linkSelector)) {
-			resolve(undefined);
-			return;
+export async function loadComponentCss(tagName: string, stylesUrl: string) {
+	const linkPromise = new Promise((resolve, reject) => {
+		const linkSelector = `link[rel="stylesheet"][data-component="${tagName}"]`;
+		let existingLink = document.head.querySelector<HTMLLinkElement>(linkSelector);
+
+		if (!existingLink) {
+			document.head.insertAdjacentHTML(
+				'beforeend',
+				/* html */ `
+				<link
+					rel="stylesheet"
+					fetchpriority="low"
+					data-component="${tagName}"
+					href="${stylesUrl}"
+				/>
+			`
+			);
+
+			existingLink = document.head.querySelector<HTMLLinkElement>(linkSelector);
 		}
 
-		document.head.insertAdjacentHTML(
-			'beforeend',
-			/* html */ `
-			<link
-				rel="stylesheet"
-				fetchpriority="low"
-				data-component="${tagName}"
-				href="${stylesUrl}"
-			/>
-		`
-		);
-
-		document.head.querySelector(linkSelector)?.addEventListener('load', () => {
+		existingLink?.addEventListener('load', () => {
 			resolve(undefined);
 		});
 
-		document.head.querySelector(linkSelector)?.addEventListener('error', () => {
+		existingLink?.addEventListener('error', () => {
 			reject(new Error('Error loading component CSS'));
 		});
 	});
+
+	const timeoutPromise = new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(undefined);
+		}, LOADING_TIMEOUT);
+	});
+
+	return Promise.race([
+		linkPromise,
+		timeoutPromise
+	]);
 }
