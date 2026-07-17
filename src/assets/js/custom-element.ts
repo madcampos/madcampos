3b95cdf25b393d9543a7eefc710bbb019db109ae
madcampos <609904+madcampos@users.noreply.github.com>
2026-07-17 02:07:51 -0400

    fix: custom element logic

--- src/assets/js/custom-element.ts
+++ src/assets/js/custom-element.ts
@@ -2,26 +2,29 @@ export async function loadComponentCss(tagName: string, stylesUrl: string) {
 	return new Promise((resolve, reject) => {
 		const linkSelector = `link[rel="stylesheet"][data-component="${tagName}"]`;
 
-		if (!document.head.querySelector(linkSelector)) {
-			document.head.insertAdjacentHTML(
-				'beforeend',
-				/* html */ `
-				<link
-					rel="stylesheet"
-					fetchpriority="low"
-					data-component="${tagName}"
-					href="${stylesUrl}"
-				/>
-			`
-			);
-
-			document.head.querySelector(linkSelector)?.addEventListener('load', () => {
-				resolve(undefined);
-			});
-
-			document.head.querySelector(linkSelector)?.addEventListener('error', () => {
-				reject(new Error('Error loading component CSS'));
-			});
+		if (document.head.querySelector(linkSelector)) {
+			resolve(undefined);
+			return;
 		}
+
+		document.head.insertAdjacentHTML(
+			'beforeend',
+			/* html */ `
+			<link
+				rel="stylesheet"
+				fetchpriority="low"
+				data-component="${tagName}"
+				href="${stylesUrl}"
+			/>
+		`
+		);
+
+		document.head.querySelector(linkSelector)?.addEventListener('load', () => {
+			resolve(undefined);
+		});
+
+		document.head.querySelector(linkSelector)?.addEventListener('error', () => {
+			reject(new Error('Error loading component CSS'));
+		});
 	});
 }
